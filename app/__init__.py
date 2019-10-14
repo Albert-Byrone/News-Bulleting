9a57b5aae0bf3d2bef2503d1007af50755a4b0ce
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-14 18:42:40 +0300

    add an app instance to the file

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from config import config_options
 from flask_bootstrap import Bootstrap
 
 def create_app(config_name):
-app = Flask(__name__)
+    app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
 
@@ -19,4 +19,6 @@ app = Flask(__name__)
     configure_request(app)
 
     return app
+
+
 
