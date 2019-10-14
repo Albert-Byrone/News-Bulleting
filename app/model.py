9791ee4865fcdae0dc2c8a61f8e99d857074916e
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-14 19:00:07 +0300

    add an article class

---
+++ app/model.py
@@ -0,0 +1,13 @@
+class Articles:
+    '''
+    A class that defines the article object
+    '''
+    def __init__(self,source,author,title,description,url,urlToImage,publishedAt):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+
