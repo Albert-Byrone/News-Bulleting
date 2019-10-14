9fe172424d9764afa396046035d273630a86e68a
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-14 18:42:05 +0300

    create app instance that returns the app

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,21 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 
+def create_app(config_name):
 app = Flask(__name__)
 
-from app import views
+    app.config.from_object(config_options[config_name])
+
+     # Initializing flask extensions
+    # bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+    return app
+
