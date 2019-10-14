543d0fe73dc19a4cbb77395ffc7cab443bd07512
Albert Byrone <albertbyrone1677@gmail.com>
2019-10-14 19:00:19 +0300

    create asources class

--- app/model.py
+++ app/model.py
@@ -11,3 +11,15 @@ class Articles:
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
 
+
+class Sources:
+    def __init__(self,id,name,description,url,category,country,language):
+        '''
+        '''
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
